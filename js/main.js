bc0d205ea859d914e3e050209f869b2b52bb929a
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-04 14:26:17 +0200

    file push

---
+++ js/main.js
@@ -0,0 +1,35 @@
+class Passenger {
+    constructor (km, age) {
+        this.km = km;
+        this.age = age;
+    }
+}
+
+// object creation
+
+var pass1 = new Passenger();
+
+// km prompt
+
+// pass1.km = parseInt(prompt("quanti chilometri vuoi percorrere?"));
+
+// age prompt
+
+// var correct = false; 
+
+// while (!correct) {
+//     pass1.age = parseInt(prompt("quanti anni hai?"));
+
+//     if (pass1.age >= 0 && pass1.age <= 110) {
+//         correct = true;
+//         console.log("age accepted");
+//     } else {
+//         console.log("not valid number in age field");
+//     }
+// }
+
+// price calc
+
+if (pass1.age > 65) {
+
+}
